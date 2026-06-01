6ce7a8d09ace6773e9a999bbea8c30dcd0b7f654
Bryanrobu <1211898@student.roc-nijmegen.nl>
2026-06-01 14:15:03 +0200

    Added authentication system
    You can now login (and register) with both google, and email

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -20,3 +21,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export {db}; // Export db to use in other parts of your app
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Footer from "./partials/footer.jsx"
 import Home from "./pages/home.jsx"
 import Review from "./pages/review.jsx"
 import Kaart from "./pages/kaart.jsx"
+import Login from "./pages/login.jsx"
+import Register from "./pages/register.jsx"
 
 function App() {
     return (<>
@@ -15,6 +17,8 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/review" element={<Review/>}/>
                 <Route path="/kaart" element={<Kaart/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
             </Routes>
         </main>
 
