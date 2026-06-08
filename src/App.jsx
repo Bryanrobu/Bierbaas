58ba0ad8557bb63d3b6454aff5b248a37c6b693d
cas <caswenting@gmail.com>
2026-06-08 14:03:43 +0200

    age check made
    made an agecheck where you have to input your birthdate and it will check if you are 18+
    if not you get send to a different page and when you do not put in a correct date you get an error

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import './App.css'
-import { useState } from 'react'
-import {Route, Routes} from 'react-router-dom'
+import "./App.css";
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
-import Header from "./partials/header.jsx"
-import Footer from "./partials/footer.jsx"
-import Home from "./pages/home.jsx"
-import Review from "./pages/review.jsx"
-import MapPage from "./pages/map.jsx"
-import Login from "./pages/login.jsx"
-import Register from "./pages/register.jsx"
-import AgeVerification from "./Components/AgeCheck.jsx"
+import Header from "./partials/header.jsx";
+import Footer from "./partials/footer.jsx";
+import Home from "./pages/home.jsx";
+import Review from "./pages/review.jsx";
+import MapPage from "./pages/map.jsx";
+import Login from "./pages/login.jsx";
+import Register from "./pages/register.jsx";
+import AgeCheck from "./Components/AgeCheck.jsx";
 
 function App() {
-    const [verified, setVerified] = useState(
-        localStorage.getItem('age_verified') === 'true'
-    )
+  const [verified, setVerified] = useState(
+    localStorage.getItem("age_verified") == "true",
+  );
 
-    return (<>
-        {!verified && <AgeVerification onVerified={() => setVerified(true)} />}
+  return (
+    <>
+      {!verified && <AgeCheck onVerified={() => setVerified(true)} />}
 
-        <Header />
-        <main>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/review" element={<Review />} />
-                <Route path="/map" element={<MapPage />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </main>
-        <Footer />
-    </>)
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
